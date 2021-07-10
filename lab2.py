684fe30bd37ece0f7529feb62a829c9496c2202e
AshmitaPersaud <86687809+AshmitaPersaud@users.noreply.github.com>
2021-07-10 15:49:40 +0000

    Completed

--- lab2.py
+++ lab2.py
@@ -4,25 +4,24 @@
 once = "umbr"
 repeat = "ella"
 
-rihanna_says = ...
-
+rihanna_says = (once + repeat + (' ' + repeat)*2)
+print (rihanna_says)
 # Hint: Use both the addition and multiplication signs to make your code concise
 
 # 2.
 # Find values for x such that each print statement is reached.
+for x in range(-50, 500):
+ y = 5
 
-x = ...
-y = 5
-
-if -1 < x / y < 1:
+ if -1 < x / y < 1:
   if not x:
     print("Found me! x is ", x)
   else:
     print("Found me two! x is ", x)
-elif x > 2:
+ elif x > 2:
   if x >= 3 and x - y <= 2:
     print("Found me three! x is", x)
-else:
+ else:
   if x % y > 1:
     print("Found me four! x is", x)
 
@@ -32,18 +31,16 @@ else:
 my_variable = 0
 for i in range(1, 10):
   # TODO add i to my_variable
-  ...
+  my_variable += i
 
   # TODO check if my_variable is even
-  if ...:
+  if my_variable %2==0:
+    my_variable = my_variable**2
     # TODO square my_variable
-    ...
   else:
+    my_variable /= 10
     # TODO divide my_variable by 10
-    ...
-
   # TODO check if my_variable is greater than 100
-  if ...:
+  if my_variable > 100:
     break
-
 print(my_variable)
